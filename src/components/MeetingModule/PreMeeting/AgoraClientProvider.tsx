2129999f4dc5323e3f84ed4ee15c6861e8efecba
Shahid Bagwan <spread.inc.business@gmail.com>
2024-11-19 17:26:25 +0530

    client initialization changed

--- src/components/MeetingModule/PreMeeting/AgoraClientProvider.tsx
+++ src/components/MeetingModule/PreMeeting/AgoraClientProvider.tsx
@@ -1,25 +1,17 @@
 "use client";
-import React, { useRef } from "react";
-import AgoraRTC, { AgoraRTCProvider, IAgoraRTCClient } from "agora-rtc-react";
+import React from "react";
+import AgoraRTC, { AgoraRTCProvider } from "agora-rtc-react";
 
 export default function AgoraClientProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const client = useRef<IAgoraRTCClient>(null);
-
-  if (typeof window !== "undefined" && !client.current) {
-    // @ts-expect-error client.current is null
-    client.current = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
-    console.log("client initialized", client.current);
-  }
-
-  if (!client.current) {
-    return null; // Or render a loading state if desired
-  }
-
   return (
-    <AgoraRTCProvider client={client.current}>{children}</AgoraRTCProvider>
+    <AgoraRTCProvider
+      client={AgoraRTC.createClient({ mode: "rtc", codec: "vp8" })}
+    >
+      {children}
+    </AgoraRTCProvider>
   );
 }
